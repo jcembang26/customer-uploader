7f17e6d02ff6cdbbe941767f39b223f7bfcf7490
JR Embang <johnrobertembang@gmail.com>
2025-06-12 19:13:06 +0800

    remove jwt

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,5 @@ use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\TokenController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/create-token', [TokenController::class, 'createToken']);
-// Route::middleware('auth:api')->group(function () {
-    Route::get('/customers', [CustomerController::class, 'list']);
-    Route::get('/customers/{id}', [CustomerController::class, 'show']);
-// });
+Route::get('/customers', [CustomerController::class, 'list']);
+Route::get('/customers/{id}', [CustomerController::class, 'show']);
